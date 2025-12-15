9e1b7d589ba1bfdfdd1e0b814c7f7ea5c9af5cae
shampoo6 <shampoo6@163.com>
2025-12-15 11:51:06 +0800

    添加表格排序功能：支持点击表头排序，带自定义排序指示器；添加排序开关控制，确保仅在表格加载完成后可用；优化排序指示器显示位置

--- widgets/file_table.py
+++ widgets/file_table.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-from PySide6.QtCore import Slot, QTimer, Signal
+from PySide6.QtCore import Slot, QTimer, Signal, Qt
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QTableWidget, QHeaderView, QCheckBox, QTableWidgetItem, QMenu, QApplication
+from PySide6.QtWidgets import QTableWidget, QHeaderView, QCheckBox, QTableWidgetItem, QMenu, QApplication, QStyle
 
-from utils import byte_size_to_str
+from utils import byte_size_to_str, file_size_to_byte
 
 
 class FileTable(QTableWidget):
@@ -19,6 +19,47 @@ class FileTable(QTableWidget):
         self.first_batch_size = 1000
         self.batch_size = 3
         self.cellClicked.connect(self.on_cell_clicked)
+        
+        # 存储原始数据用于排序
+        self.original_data = []
+        # 当前列排序状态 (列索引, 排序顺序)
+        self.sort_column = -1
+        self.sort_order = Qt.AscendingOrder
+        # 排序功能开关，只有在表格加载完成后才能使用
+        self.sort_enabled = False
+        
+        # 启用表头排序
+        self.horizontalHeader().setSectionsClickable(True)
+        self.horizontalHeader().sectionClicked.connect(self.on_header_clicked)
+        
+        # 获取排序指示器图标
+        self.style = QApplication.style()
+        self.sort_ascending_icon = self.style.standardIcon(QStyle.SP_ArrowUp)
+        self.sort_descending_icon = self.style.standardIcon(QStyle.SP_ArrowDown)
+        
+        # 设置表头样式，将排序指示器放在右侧
+        self.horizontalHeader().setStyleSheet("""
+            QHeaderView::section {
+                /*padding-right: 20px;   为排序指示器留出空间 */
+                text-align: left;
+                border: 1px solid #c0c0c0;
+                background-color: #f0f0f0;
+            }
+            QHeaderView::down-arrow {
+                image: url(:/icons/arrow-down-bold.png);
+                subcontrol-position: right;
+                right: 5px;
+                width: 12px;
+                height: 12px;
+            }
+            QHeaderView::up-arrow {
+                image: url(:/icons/arrow-up-bold.png);
+                subcontrol-position: right;
+                right: 5px;
+                width: 12px;
+                height: 12px;
+            }
+        """)
 
     def contextMenuEvent(self, e) -> None:
         p = e.pos()
@@ -85,6 +126,8 @@ class FileTable(QTableWidget):
         if len(batch_idx) > 0:
             QTimer.singleShot(0, lambda: self._build_next_batch(batch_idx, table_datas))
         else:
+            # 表格加载完成，启用排序功能
+            self.sort_enabled = True
             self.loaded.emit(True)
 
     def _build_first_batch(self, table_datas):
@@ -98,6 +141,10 @@ class FileTable(QTableWidget):
         _first_batch_datas = table_datas[:_first_batch_count]
 
         self._build_table(_first_batch_datas)
+        
+        # 设置表头标签
+        self.setHorizontalHeaderLabels(['', '相对路径', '绝对路径', '是否目录', '扩展名', '大小'])
+        
         self.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
         self.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
         self.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
@@ -116,12 +163,25 @@ class FileTable(QTableWidget):
             batch_idx = [i for i in range(0, row_count, self.batch_size)]
             self._build_next_batch(batch_idx, table_datas)
         else:
+            # 表格加载完成，启用排序功能
+            self.sort_enabled = True
             self.loaded.emit(True)
 
     def load_table(self, cancel_event, table_datas):
         self.cancel_event = cancel_event
         self.clearContents()
         self.setRowCount(0)
+        
+        # 关闭排序功能
+        self.sort_enabled = False
+        
+        # 保存原始数据用于排序
+        self.original_data = table_datas.copy()
+        # 重置排序状态
+        self.sort_column = -1
+        self.sort_order = Qt.AscendingOrder
+        # 清除排序指示器
+        self._update_sort_indicator()
 
         if self.cancel_event.is_set():
             self.loaded.emit(False)
@@ -169,3 +229,98 @@ class FileTable(QTableWidget):
         for i in range(self.rowCount()):
             checkbox: QCheckBox = self.cellWidget(i, 0)
             checkbox.setChecked(False)
+    
+    @Slot(int)
+    def on_header_clicked(self, column):
+        """处理表头点击事件，实现排序功能"""
+        # 检查排序功能是否启用
+        if not self.sort_enabled:
+            return
+            
+        # 复选框列不排序
+        if column == 0:
+            return
+            
+        # 如果点击的是同一列，则切换排序顺序
+        if self.sort_column == column:
+            self.sort_order = Qt.DescendingOrder if self.sort_order == Qt.AscendingOrder else Qt.AscendingOrder
+        else:
+            self.sort_column = column
+            self.sort_order = Qt.AscendingOrder
+        
+        # 对原始数据进行排序
+        sorted_data = self._sort_data(self.original_data, column, self.sort_order)
+        
+        # 重新加载表格
+        self._reload_table(sorted_data)
+    
+    def _sort_data(self, data, column, order):
+        """根据列和排序顺序对数据进行排序"""
+        reverse = (order == Qt.DescendingOrder)
+        
+        if column == 1:  # 相对路径列
+            return sorted(data, key=lambda x: x[1], reverse=reverse)
+        elif column == 2:  # 绝对路径列
+            return sorted(data, key=lambda x: x[2], reverse=reverse)
+        elif column == 3:  # 是否目录列
+            return sorted(data, key=lambda x: x[3], reverse=reverse)
+        elif column == 4:  # 扩展名列
+            return sorted(data, key=lambda x: x[4], reverse=reverse)
+        elif column == 5:  # 大小列
+            return sorted(data, key=lambda x: x[5], reverse=reverse)
+        else:
+            return data
+    
+    def _reload_table(self, sorted_data):
+        """重新加载表格数据"""
+        # 保存当前选中状态
+        checked_items = set()
+        for i in range(self.rowCount()):
+            checkbox: QCheckBox = self.cellWidget(i, 0)
+            if checkbox.isChecked():
+                abs_path = self.item(i, 2).text()
+                checked_items.add(abs_path)
+        
+        # 清空表格
+        self.clearContents()
+        self.setRowCount(0)
+        
+        # 重新构建表格
+        self._build_table(sorted_data)
+        
+        # 设置表头标签
+        self.setHorizontalHeaderLabels(['', '相对路径', '绝对路径', '是否目录', '扩展名', '大小'])
+        
+        # 恢复选中状态
+        for i in range(self.rowCount()):
+            abs_path = self.item(i, 2).text()
+            if abs_path in checked_items:
+                checkbox: QCheckBox = self.cellWidget(i, 0)
+                checkbox.setChecked(True)
+        
+        # 更新列宽
+        self.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(2, QHeaderView.ResizeMode.Stretch)
+        self.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(4, QHeaderView.ResizeMode.ResizeToContents)
+        self.horizontalHeader().setSectionResizeMode(5, QHeaderView.ResizeMode.ResizeToContents)
+        
+        # 更新排序指示器
+        self._update_sort_indicator()
+    
+    def _update_sort_indicator(self):
+        """更新表头排序指示器"""
+        header = self.horizontalHeader()
+        
+        # 确保表头可点击
+        header.setSectionsClickable(True)
+        
+        # 如果有排序列，显示排序指示器
+        if self.sort_column >= 0:
+            header.setSortIndicator(self.sort_column, self.sort_order)
+            # 确保显示排序指示器
+            header.setSortIndicatorShown(True)
+        else:
+            # 隐藏排序指示器
+            header.setSortIndicatorShown(False)
